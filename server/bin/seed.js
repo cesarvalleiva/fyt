9e58f20c67783faeb3c01695fcfd52d18eca87cb
cesarvalleiva <cesarvalleiva@gmail.com>
2020-03-10 15:58:37 +0100

    chabge db on seed file

--- server/bin/seed.js
+++ server/bin/seed.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 function dbConnect(cb) {
   mongoose
-    .connect(`${process.env.DB_LOCAL}`, {
+    .connect(`${process.env.DB_URL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
